bfa207775d0a4d99a0f8ee28ee42a496a9addd90
Italo <italoramirezp@gmail.com>
2021-06-30 16:37:52 -0500

    Avanza la prueba y se completa el creud de post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Posts;
 use App\Form\PostType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class PostController extends AbstractController
@@ -32,7 +34,7 @@ class PostController extends AbstractController
                         $newFilename
                     );
                 } catch (FileException $e) {
-                    throw new \Exception('UPs! ha ocurrido un error, sorry :c');
+                    throw new \Exception('ha ocurrido un error, sorry :c');
                 }
                 $post->setFoto($newFilename);
             }
@@ -68,15 +70,70 @@ class PostController extends AbstractController
     /**
      * @Route("/my-post/", name="allPost")
      */
-    public function myPost()
+    public function myPost(PaginatorInterface $paginator, Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
-        $post = $em->getRepository(Posts::class)->findBy(['user' => $user]);
-        return $this->render('post/myPost.html.twig', ['post' => $post]);
+        $query = $em->getRepository(Posts::class)->findBy(['user' => $user]);
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 2), /*page number*/
+            4 /*limit per page*/
+        );
+        return $this->render('post/myPost.html.twig', [
+            'post' => $query,
+            'pagination' => $pagination
+        ]);
     }
 
-    public function update()
+    /**
+     * @Route("/{id}/edit-post", name="editPost")
+     */
+
+    public function edit(Posts $posts, Request $request)
     {
+        $form = $this->createForm(PostType::class, $posts);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $File = $form['foto']->getData();
+            if ($File) {
+                $originalFilename = pathinfo($File->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $File->guessExtension();
+                try {
+                    $File->move(
+                        $this->getParameter('photos_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    throw new \Exception('ha ocurrido un error, sorry :c');
+                }
+                $posts->setFoto($newFilename);
+            }
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('allPost');
+        }
+
+        return $this->render('post/editPost.html.twig', [
+            'posts' => $posts,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="deletePost", methods={"POST"})
+     */
+
+    public function delete(Posts $post, Request $request): Response
+    {
+        if ($this->isCsrfTokenValid('deleted' . $post->getId(),     $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('allPost');
     }
 }
